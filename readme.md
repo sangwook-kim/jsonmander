54a826935fffa80d99a5c9c3b7a06bde0a409b9f
sangwook kim <sangwook.kim@navercorp.com>
2016-05-06 08:29:07 +0900

    making object to work

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # json browser
 displays tree structure and a search box
+
+inspired by [json-tree by lmenezes](https://github.com/lmenezes/json-tree)
--- src/jsonmander.js
+++ src/jsonmander.js
@@ -1,33 +1,84 @@
 //(function(global) {
+    //TODO: search box - fixed position?
     var rootEl = document.createElement('ul'),
+        escapeMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            '\'': '&#x27;',
+            '/': '&#x2F;'
+        },
+        escapeString = function(str) {
+            return str.replace(/[&<>'"]/g, function(c) {
+                return escapeMap[c];
+            });
+        },
         jsonmander = function(json) {
             rootEl.className = '_jsonmander';
     
-            console.log( describe(json, 0) );
-    
-            rootEl.innerHTML = JSON.stringify(json);
+            rootEl.innerHTML = describe(json);
+
             return rootEl;
         },
-        describe = function(val, depth) {
+        describe = function(val, depth, index) {
             if(val !== null) {
+                var type = typeof val;
+                switch (type) {
+                  case 'boolean':
+                    return describePrimitive(val, '_json_bool', depth, index);
+                  case 'number':
+                    return describePrimitive(val, '_json_number', depth, index);
+                  case 'string':
+                    return describePrimitive(escapeString(val), '_json_string', depth, index);
+                  default:
+                    if (val instanceof Array) {
+                      return describeArray(val, depth, indent, index);
+                    } else {
+                      return describeObject(val, depth, indent, index);
+                    }
+                }
             } else {
-                return describeNull(indent ? depth: 0);
+                return describePrimitive(val, '_json_null', depth, index);
             }
         },
-        describeNull = function(depth, index) {
-            var row = getRow(),
+        describePrimitive = function(val, className, depth, index) {
+            var depth = depth ? depth: 0,
                 content;
 
             if(typeof index !== 'undefined') {
+                content = '<span class="_json_index">' +
+                              pushRight(depth) + '[' + index + ']' +
+                          '</span>' +
+                          '<span class="_json_val ' + className + '">' +
+                              ' ' + val +
+                          '</span>';
             } else {
+                content = '<span class="_json_val ' + className + '">' +
+                              pushRight(depth) + val +
+                          '</span>';
             }
-            return row;
-        },
-        getRow = function(depth) {
-            var rowEl = document.creatElement('li');
-            rowEl.className = '_jsonmander_row';
 
-            return rowEl;
+            return '<li class="_jsonmander_row">' + content + '</li>';
+ 
+        },
+        describeObject = function(val, depth, index, indent) {
+            //TODO: init fold flag?
+            var depth = depth ? depth: 0,
+                content = Object.keys(val).map(function(key) {
+                              return '<div class="_json_obj"><span class="_json_key">' +
+                                     pushRight(depth + 1) + '"' + key + '": ' +
+                                     '</span>' + describe(val[key]);
+                          }).join('</div>');
+    /*
+    var body = [
+      _openBracket('{', indent ? depth : 0, id),
+      _span(content, {id: id}),
+      _closeBracket('}', depth)
+    ].join('\n');
+    return _span(body, {})
+    */
+            return content;
         },
         pushRight = function(depth) {
             return Array((depth * 2) + 1).join(' ');
