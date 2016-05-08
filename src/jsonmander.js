a38df48477029439c309c8003b4ebf9c09991473
sangwook kim <sangwook.kim@navercorp.com>
2016-05-08 10:24:33 +0900

    fold

--- src/jsonmander.js
+++ src/jsonmander.js
@@ -1,6 +1,10 @@
-//(function(global) {
+//TODO: text search - rootEl.childNodes[i].textContent
+(function(global) {
     //TODO: search box - fixed position?
+    //TODO: multiple instance rootEl;
     var rootEl = document.createElement('ul'),
+        instance = 0, _id = 0,
+        originalJson, originalTree,
         escapeMap = {
             '&': '&amp;',
             '<': '&lt;',
@@ -15,12 +19,17 @@
             });
         },
         jsonmander = function(json) {
+            ++instance;
             depth = 0;
+            originalJson = json;
             var rows = describe(json);
             rows = parseRows(rows);
 
             rootEl.className = '_jsonmander';
             rootEl.innerHTML = rows;
+
+            originalTree = rootEl;
+            rootEl.addEventListener('click', toggleFold);
 
             return rootEl;
         },
@@ -29,7 +38,12 @@
             rows = rows.split('&');
             for(var i = 0, l = rows.length; i < l; i++) {
                 if(rows[i] !== '') {
-                    parsedRows.push('<li class="_jsonmander_row">' + rows[i] + '</li>');
+                    if(rows[i].split('data-block-id="').length > 1) {
+                        var foldID = 'jsonmander_' + rows[i].split('data-block-id="')[1].split('"')[0];
+                        parsedRows.push('<li id="' + foldID + '" class="_jsonmander_row">' + rows[i] + '</li>');
+                    } else {
+                        parsedRows.push('<li class="_jsonmander_row">' + rows[i] + '</li>');
+                    }
                 }
             }
 
@@ -81,17 +95,18 @@
 
             var content = Object.keys(val).map(function(key) {
                               return '<span class="_json_key">' +
-                                     pushRight(depth) + '"' + key + '": ' +
+                                     pushRight(depth) + '"' + escapeString(key) + '": ' +
                                      '</span>' + describe(val[key]);
-                          }).join('');
+                          }).join(''),
+                objectID = instance + '_' + _id++;
 
             if(typeof index !== 'undefined') {
                 content = '<span class="_json_index">' + '[' + index + ']' + '</span>' +
-                          '<span class="_jsonmander_brace"> {</span>' +
+                          '<span class="_jsonmander_brace" data-block-id="open_' + objectID + '"> {</span>'  +
                           '<span class="_jsonmander_fold"><a href="#">+</a></span>&' +
                           content;
             } else {
-                content = '<span class="_jsonmander_brace">' +
+                content = '<span class="_jsonmander_brace" data-block-id="open_' + objectID + '">'  +
                           '{</span><span class="_jsonmander_fold"><a href="#">+</a></span>&' +
                           content;
             }
@@ -99,15 +114,15 @@
             depth--;
 
             content = content + 
-                      '<span class="_jsonmander_brace">'  + pushRight(depth) +
-                      '}</span>&';
+                      '<span class="_jsonmander_brace" data-block-id="close_' + objectID + '">'  +
+                      pushRight(depth) + '}</span>&';
 
             return content;
         },
         describeArray = function(val, index) {
             //TODO: init fold flag?
             depth++;
-            var content = '';
+            var content = '', arrayID = instance + '_' + _id++;
 
             for(var i = 0, l = val.length; i < l; ++i) {
                 content += '<span class="_json_index">' + 
@@ -117,11 +132,11 @@
 
             if(typeof index !== 'undefined') {
                 content = '<span class="_json_index">' + '[' + index + ']' + '</span>' +
-                          '<span class="_jsonmander_brace"> [</span>' +
+                          '<span class="_jsonmander_brace" data-block-id="open_' + arrayID + '"> [</span>' +
                           '<span class="_jsonmander_fold"><a href="#">+</a></span>&' +
                           content;
             } else {
-                content = '<span class="_jsonmander_brace">' +
+                content = '<span class="_jsonmander_brace" data-block-id="open_' + arrayID + '">' +
                           '[</span><span class="_jsonmander_fold"><a href="#">+</a></span>&' +
                           content;
             }
@@ -129,15 +144,35 @@
             depth--;
 
             content = content + 
-                      '<span class="_jsonmander_brace">'  + pushRight(depth) +
-                      ']</span>&';
+                      '<span class="_jsonmander_brace" data-block-id="close_' + arrayID + '">'  +
+                      pushRight(depth) + ']</span>&';
 
             return content;
         },
         pushRight = function(depth) {
             return Array((depth * 2) + 1).join(' ');
         },
+        toggleFold = function(e) {
+            if(e.target && e.target.nodeName === 'A') {
+                e.preventDefault();
+                //TODO: toggle
+                var openLi = e.target.parentNode.parentNode,
+                    closeID = openLi.id.replace('open', 'close'),
+                    bankEl = document.createElement('ul'),
+                    nextLi = openLi.nextSibling;
+
+                bankEl.className = '_jsonmander_bank ' + closeID;
+                while(nextLi.id !== closeID) {
+                    var oldLi = nextLi;
+                    nextLi = nextLi.nextSibling;
+                    oldLi.remove();
+                    bankEl.appendChild(oldLi);
+                }
+                rootEl.appendChild(bankEl);
+                debugger;
+            }
+        },
         depth;
 
-//    global.jsonmander = jsonmander;
-//})(this);
+    global.jsonmander = jsonmander;
+})(this);
